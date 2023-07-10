2185e6128b2cfa7220db7cbdec0ebf04182994fc
JJiGolem <lorsanov2000@gmail.com>
2023-07-10 22:32:24 +0300

    Fix output about the state of the vehinfo script

--- client_packages/vinfo/index.js
+++ client_packages/vinfo/index.js
@@ -1,43 +1,45 @@
-let dlEnabled = false;
-
 const NativeUI = require("nativeui");
 const UIMenuCheckboxItem = NativeUI.UIMenuCheckboxItem;
 
-let vehInfoItem = new UIMenuCheckboxItem("Vehicle Info", false);
+let dlEnabled = false;
+
+const vehInfoItem = new UIMenuCheckboxItem("Vehicle Info", false);
 dr.driftMenu.AddItem(vehInfoItem);
 dr.driftMenu.CheckboxChange.on((item, checked) => {
-    if (item == vehInfoItem) {
-        dlEnabled = checked;
-    }
-
-    if(dlEnabled)
-        mp.gui.chat.push("!{#0099ff}[Vehicle Info] !{White}Showing.");
-    else
-        mp.gui.chat.push("!{#0099ff}[Vehicle Info] !{White}Hiding.");
+  if (item == vehInfoItem) {
+    dlEnabled = checked;
+    const notifyMessage = `<font color="#0099ff">[Vehicle Info]</font> ${dlEnabled ? "~w~Showing" : "~c~Hidding"}`;
+    mp.game.graphics.notify(notifyMessage);
+  }
 });
 
 mp.events.add("render", () => {
-    if(dlEnabled)
-    {
-        mp.vehicles.forEachInStreamRange((vehicle) => { 
-            if(mp.players.local.position.subtract(vehicle.position).length() < 10)
-            {
-                const drawPosition = [vehicle.position.x, vehicle.position.y, vehicle.position.z + 0.3];
-                mp.game.graphics.drawText(`~b~Id: ~w~${vehicle.remoteId}\n~b~Model: ~w~${mp.game.ui.getLabelText(mp.game.vehicle.getDisplayNameFromVehicleModel(vehicle.model))}\n~b~Position: ~w~${vehicle.position.x.toFixed(2)}, ${vehicle.position.y.toFixed(2)}, ${vehicle.position.z.toFixed(2)}\n`, drawPosition, { 
-                    font: 0, 
-                    color: [255, 255, 255, 185], 
-                    scale: [0.25, 0.25], 
-                    outline: true,
-                    centre: false
-                });
-                mp.game.graphics.drawText(`\n\n\n~b~Heading: ~w~${vehicle.getHeading().toFixed(2)}\n~b~Health: ~w~${vehicle.getHealth()}`, drawPosition, { 
-                    font: 0, 
-                    color: [255, 255, 255, 185], 
-                    scale: [0.25, 0.25], 
-                    outline: true,
-                    centre: false
-                });
-            }
-        }); 
+  if (!dlEnabled) {
+    return;
+  }
+
+  mp.vehicles.forEachInStreamRange((vehicle) => {
+    if (mp.players.local.position.subtract(vehicle.position).length() >= 10) {
+      return;
     }
+
+    const drawPosition = [vehicle.position.x, vehicle.position.y, vehicle.position.z + 0.3];
+    const displayName = mp.game.vehicle.getDisplayNameFromVehicleModel(vehicle.model);
+    const labelText = mp.game.ui.getLabelText(displayName);
+    const modelName = labelText == "NULL" ? displayName : labelText;
+    mp.game.graphics.drawText(`~b~Id: ~w~${vehicle.remoteId}\n~b~Model: ~w~${modelName}\n~b~Position: ~w~${vehicle.position.x.toFixed(2)}, ${vehicle.position.y.toFixed(2)}, ${vehicle.position.z.toFixed(2)}\n`, drawPosition, {
+      font: 0,
+      color: [255, 255, 255, 185],
+      scale: [0.25, 0.25],
+      outline: true,
+      centre: false
+    });
+    mp.game.graphics.drawText(`\n\n\n~b~Heading: ~w~${vehicle.getHeading().toFixed(2)}\n~b~Health: ~w~${vehicle.getHealth()}`, drawPosition, {
+      font: 0,
+      color: [255, 255, 255, 185],
+      scale: [0.25, 0.25],
+      outline: true,
+      centre: false
+    });
+  });
 });
