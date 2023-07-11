df209f19f32054727bee09236127c18a5c9285f2
JJiGolem <lorsanov2000@gmail.com>
2023-07-11 03:24:33 +0300

    Refactor menus open way for vspawn, vtuning

--- client_packages/vspawner/index.js
+++ client_packages/vspawner/index.js
@@ -5,76 +5,59 @@ const Point = NativeUI.Point;
 
 const vehicles = require("vspawner/vehicleHashes");
 const customVehicles = require("vspawner/dlcVehicleNames")
-const categoryTitles = ["Compacts", "Sedans", "SUVs", "Coupes", "Muscle", "Sports Classics", "Sports", "Super", "Motorcycles", "Off-Road", "Industrial", "Utility", "Vans", "Cycles", "Boats", "Helicopters", "Planes", "Service", "Emergency", "Military", "Commercial", "Trains"];
+const categoryTitles = ["Compacts", "Sedans", "SUVs", "Coupes", "Muscle", "Sports Classics", "Sports", "Super", "Motorcycles", "Off-Road", "Industrial", "Utility", "Vans", "Cycles", "Boats", "Helicopters", "Planes", "Service", "Emergency", "Military", "Commercial", "Trains", "Open Wheel"];
 
 // main menu
 const mainMenu = new Menu("Vehicle Spawner", "", new Point(1250, 150));
 mainMenu.Visible = false;
 
-mainMenu.ItemSelect.on((item, index) => {
-    mainMenu.Visible = false;
-    curCategory = index;
-    categoryMenus[index].Visible = true;
-    transition = true;
-});
-
 let categoryMenus = [];
-let curCategory = -1;
-let transition = false;
 
 // categories
 function addMainMenuCategory(categoryName) {
-    mainMenu.AddItem(new UIMenuItem(categoryName, ""));
-    let categoryMenu = new Menu(categoryName, "", new Point(1250, 150));
-    categoryMenu.Visible = false;
-    
-    categoryMenu.ItemSelect.on((item, index) => {
-        if (!transition) mp.events.callRemote("vspawner_Spawn", item.Text);
-        transition = false;
-    });
-    
-    categoryMenu.MenuClose.on(() => {
-        curCategory = -1;
-        mainMenu.Visible = true;
-    });
-    
-    categoryMenus.push(categoryMenu);
+  const categoryItem = new UIMenuItem(categoryName);
+  const categoryMenu = new Menu(categoryName, "", new Point(1250, 150));
+  categoryMenu.Visible = false;
+
+  categoryMenu.ItemSelect.on((item, index) => {
+    mp.events.callRemote("vspawner_Spawn", item.Text);
+  });
+
+  categoryMenus.push(categoryMenu);
+
+  mainMenu.AddItem(categoryItem);
+  mainMenu.BindMenuToItem(categoryMenu, categoryItem);
 }
 
 // Custom Vehicles from DLC
 addMainMenuCategory("Custom Vehicles")
 for (const index in customVehicles) {
-    const modelName = customVehicles[index];
-    const modelHash = mp.game.joaat(modelName);
-    const displayName = mp.game.vehicle.getDisplayNameFromVehicleModel(modelHash);
-    const vehicleItem = new UIMenuItem(modelName, "");
-    vehicleItem.SetRightLabel(displayName ? "" : displayName);
-    categoryMenus[0].AddItem(vehicleItem);
+  const modelName = customVehicles[index];
+  const modelHash = mp.game.joaat(modelName);
+  const displayName = mp.game.vehicle.getDisplayNameFromVehicleModel(modelHash);
+  const vehicleItem = new UIMenuItem(modelName);
+  vehicleItem.SetRightLabel(displayName ? "" : displayName);
+  categoryMenus[0].AddItem(vehicleItem);
 }
 
 for (let i = 0; i < categoryTitles.length; i++) {
-    addMainMenuCategory(categoryTitles[i]);
+  addMainMenuCategory(categoryTitles[i]);
 }
 
 // vehicles
 for (let prop in vehicles) {
-    if (vehicles.hasOwnProperty(prop)) {
-        let vehicleClass = mp.game.vehicle.getVehicleClassFromName(vehicles[prop]);
-        let vehicleName = mp.game.ui.getLabelText(prop);
-        let vehicleItem = new UIMenuItem(prop, "");
-        vehicleItem.SetRightLabel(vehicleName == "NULL" ? "" : vehicleName);
+  if (vehicles.hasOwnProperty(prop)) {
+    let vehicleClass = mp.game.vehicle.getVehicleClassFromName(vehicles[prop]);
+    let vehicleName = mp.game.ui.getLabelText(prop);
+    let vehicleItem = new UIMenuItem(prop, "");
+    vehicleItem.SetRightLabel(vehicleName == "NULL" ? "" : vehicleName);
 
-        // first index => Custom Vehicles
-        const fixIndex = vehicleClass + 1;
-        categoryMenus[fixIndex].AddItem(vehicleItem);
-    }
+    // first index => Custom Vehicles
+    const fixIndex = vehicleClass + 1;
+    categoryMenus[fixIndex].AddItem(vehicleItem);
+  }
 }
 
-mp.events.add("toggle_veh_spawn_menu", () =>
-{
-    if (curCategory > -1) {
-        categoryMenus[curCategory].Visible = !categoryMenus[curCategory].Visible;
-    } else {
-        mainMenu.Visible = !mainMenu.Visible;
-    }	
-});
+const menuItem = new UIMenuItem("Vehicle Spawn");
+dr.driftMenu.AddItem(menuItem);
+dr.driftMenu.BindMenuToItem(mainMenu, menuItem);
--- client_packages/vtuning/index.js
+++ client_packages/vtuning/index.js
@@ -3,57 +3,56 @@ const Menu = NativeUI.Menu;
 const UIMenuItem = NativeUI.UIMenuItem;
 const Point = NativeUI.Point;
 
-const categoryTitles = 
-[
-	"Spoiler",
-	"Front Bumper",
-	"Rear Bumper",
-	"Side Skirts",
-	"Exhaust",
-	"Rollcage",
-	"Grille",
-	"Bonnet",
-	"Fenders and Arches",
-	"Fenders",
-	"Roof",
-	"Engine",
-	"Brakes",
-	"Transmission",
-	"Horn",
-	"Suspension",
-	"Armor",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"Headlights",
-	"",
-	"",
-	"Plate Holders",
-	"Vanity Plates",
-	"Interior Trim",
-	"Ornaments",
-	"Interior Dash",
-	"Dials",
-	"Door Speakers",
-	"Leather Seats",
-	"Steering Wheels",
-	"Column Shifters",
-	"Plaques",
-	"ICE",
-	"Speakers",
-	"Hydraulics",
-	"Engine Block",
-	"Air Filters",
-	"Strut Braces",
-	"Arch Covers",
-	"Aerials",
-	"Exterior Trim",
-	"Tank",
-	"Windows",
-	"",
-	"Livery"
+const categoryTitles = [
+  "Spoiler",
+  "Front Bumper",
+  "Rear Bumper",
+  "Side Skirts",
+  "Exhaust",
+  "Rollcage",
+  "Grille",
+  "Bonnet",
+  "Fenders and Arches",
+  "Fenders",
+  "Roof",
+  "Engine",
+  "Brakes",
+  "Transmission",
+  "Horn",
+  "Suspension",
+  "Armor",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "Headlights",
+  "",
+  "",
+  "Plate Holders",
+  "Vanity Plates",
+  "Interior Trim",
+  "Ornaments",
+  "Interior Dash",
+  "Dials",
+  "Door Speakers",
+  "Leather Seats",
+  "Steering Wheels",
+  "Column Shifters",
+  "Plaques",
+  "ICE",
+  "Speakers",
+  "Hydraulics",
+  "Engine Block",
+  "Air Filters",
+  "Strut Braces",
+  "Arch Covers",
+  "Aerials",
+  "Exterior Trim",
+  "Tank",
+  "Windows",
+  "",
+  "Livery"
 ];
 
 // main menu
@@ -64,116 +63,107 @@ let transition = false;
 
 let menus = {};
 
-function createTuningMenu(vehicle)
-{
-	curCategory = -1;
-	
-	if(mainMenu)
-	{
-		mainMenu.Visible = false;
-	}
-	
-	if(menus.hasOwnProperty(vehicle.model))
-	{
-		mainMenu = menus[vehicle.model];
-		return;
-	}
-	
-	mainMenu = new Menu("Vehicle Tuning", "", new Point(1250, 150));
-	mainMenu.Visible = false;
-	
-	menus[vehicle.model] = mainMenu;
-	
-	mainMenu.vehicleModel = vehicle.model;
+function createTuningMenu(vehicle) {
+  curCategory = -1;
 
-	mainMenu.ItemSelect.on((item, index) => {
-		mainMenu.Visible = false;
-		curCategory = index;
-		categoryMenus[index].Visible = true;
-		transition = true;
-	});
+  if (mainMenu) {
+    mainMenu.Visible = false;
+  }
 
-	let categoryMenus = [];
+  if (menus.hasOwnProperty(vehicle.model)) {
+    mainMenu = menus[vehicle.model];
+    return;
+  }
 
-	// categories
-	for (let i = 0; i < categoryTitles.length; i++) 
-	{
-		let numMods = vehicle.getNumMods(i);
-		
-		if(numMods > 0 && categoryTitles[i].length > 0)
-		{
-			mainMenu.AddItem(new UIMenuItem(categoryTitles[i], ""));
+  mainMenu = new Menu("Vehicle Tuning", "", new Point(1250, 150));
+  mainMenu.Visible = false;
 
-			let categoryMenu = new Menu(categoryTitles[i], "", new Point(1250, 150));
-			categoryMenu.Visible = false;
+  menus[vehicle.model] = mainMenu;
 
-			categoryMenu.ItemSelect.on((item, index) => {
-				if (!transition) mp.events.callRemote("vtuning_set", item.modType, item.modIndex);
-				transition = false;
-			});
+  mainMenu.vehicleModel = vehicle.model;
 
-			categoryMenu.MenuClose.on(() => {
-				curCategory = -1;
-				mainMenu.Visible = true;
-			});
-			
-			categoryMenu.modType = i;
+  mainMenu.ItemSelect.on((item, index) => {
+    mainMenu.Visible = false;
+    curCategory = index;
+    categoryMenus[index].Visible = true;
+    transition = true;
+  });
 
-			for(let modIndex = -1; modIndex < numMods; modIndex++)
-			{
-				if (modIndex == -1) {
-					let vehicleItem = new UIMenuItem("Default", "");
-					vehicleItem.modType = i;
-					vehicleItem.modIndex = modIndex;
-					categoryMenu.AddItem(vehicleItem);
-					continue;
-				}
-                
-				const modLabel = vehicle.getModTextLabel(i, modIndex);
-                const labelText = typeof(modLabel) == "string" ? mp.game.ui.getLabelText(modLabel) : "NULL";
-				const vehicleName = labelText != "NULL" ? labelText : `${categoryTitles[i]} #${modIndex}`;
-				const vehicleItem = new UIMenuItem(vehicleName, "");
-				vehicleItem.modType = i;
-				vehicleItem.modIndex = modIndex;
-				categoryMenu.AddItem(vehicleItem);
-			}
+  let categoryMenus = [];
 
-			categoryMenus.push(categoryMenu);
-		}
-	}
+  // categories
+  for (let i = 0; i < categoryTitles.length; i++) {
+    let numMods = vehicle.getNumMods(i);
+
+    if (numMods > 0 && categoryTitles[i].length > 0) {
+      mainMenu.AddItem(new UIMenuItem(categoryTitles[i], ""));
+
+      let categoryMenu = new Menu(categoryTitles[i], "", new Point(1250, 150));
+      categoryMenu.Visible = false;
+
+      categoryMenu.ItemSelect.on((item, index) => {
+        if (!transition) mp.events.callRemote("vtuning_set", item.modType, item.modIndex);
+        transition = false;
+      });
+
+      categoryMenu.MenuClose.on(() => {
+        curCategory = -1;
+        mainMenu.Visible = true;
+      });
+
+      categoryMenu.modType = i;
+
+      for (let modIndex = -1; modIndex < numMods; modIndex++) {
+        if (modIndex == -1) {
+          let vehicleItem = new UIMenuItem("Default", "");
+          vehicleItem.modType = i;
+          vehicleItem.modIndex = modIndex;
+          categoryMenu.AddItem(vehicleItem);
+          continue;
+        }
+
+        const modLabel = vehicle.getModTextLabel(i, modIndex);
+        const labelText = typeof (modLabel) == "string" ? mp.game.ui.getLabelText(modLabel) : "NULL";
+        const vehicleName = labelText != "NULL" ? labelText : `${categoryTitles[i]} #${modIndex}`;
+        const vehicleItem = new UIMenuItem(vehicleName, "");
+        vehicleItem.modType = i;
+        vehicleItem.modIndex = modIndex;
+        categoryMenu.AddItem(vehicleItem);
+      }
+
+      categoryMenus.push(categoryMenu);
+    }
+  }
 }
 
+const vtMenu = new UIMenuItem("Vehicle Tuning");
+dr.driftMenu.AddItem(vtMenu);
 
-mp.events.add("toggle_veh_tuning_menu", () =>
-{
-   if (curCategory > -1) 
-	{
-        categoryMenus[curCategory].Visible = !categoryMenus[curCategory].Visible;
+dr.driftMenu.ItemSelect.on((item, index) => {
+  if (item != vtMenu) {
+    return;
+  }
+
+  dr.driftMenu.Visible = false;
+
+  if (curCategory > -1) {
+    categoryMenus[curCategory].Visible = !categoryMenus[curCategory].Visible;
+  } else {
+    if (!mainMenu || !mainMenu.Visible) {
+      let vehicle = mp.players.local.vehicle;
+
+      if (vehicle) {
+        if (!mainMenu ||
+          mainMenu.vehicleModel !== vehicle.model) {
+          createTuningMenu(vehicle);
+        }
+
+        mainMenu.Visible = true;
+      } else {
+        mp.game.graphics.notify("~R~You should be in a vehicle to access this menu.");
+      }
+    } else {
+      mainMenu.Visible = false;
     }
-	else 
-	{
-		if(!mainMenu || !mainMenu.Visible)
-		{		
-			let vehicle = mp.players.local.vehicle;
-			
-			if(vehicle)
-			{				
-				if(!mainMenu
-					|| mainMenu.vehicleModel !== vehicle.model)
-				{
-					createTuningMenu(vehicle);
-				}
-				
-				mainMenu.Visible = true;
-			}
-			else
-			{
-				mp.game.graphics.notify("~R~You should be in a vehicle to access this menu.");
-			}
-		}	
-		else
-		{
-			mainMenu.Visible = false;
-		}
-    }	
+  }
 });
