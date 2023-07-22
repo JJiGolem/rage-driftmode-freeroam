3f09b8d29016ada3d0e6a6292e2fa86a1378944a
JJiGolem <lorsanov2000@gmail.com>
2023-07-23 01:19:58 +0300

    Add http server for remote receive commands

--- packages/commands/index.js
+++ packages/commands/index.js
@@ -1,7 +1,26 @@
 // const exec = require('child_process').execFile;
 const process = require('process');
+const http = require("http");
+const url = require('url');
 
 let isRestarted = false;
+
+function restartServer(player = null) {
+  if (isRestarted) {
+    return;
+  }
+  isRestarted = true;
+
+  const broadcast = player
+    ? `!{Red}${player.name} (!{Yellow}${player.socialClub}!{Red}) !{Blue}начал рестарт сервера. Через несколько секунд сервер перезапустится`
+    : "!{Blue}Через несколько секунд сервер перезапустится";
+
+  mp.players.broadcast(broadcast);
+  setTimeout(() => {
+    const message = player ? `${player.socialClub} restarted server` : "Server restart";
+    process.exit(message);
+  }, 2_500)
+}
 
 process.on("exit", (code) => {
   console.log("Server app exit with code: " + code)
@@ -11,13 +30,23 @@ process.on('uncaughtException', function (err) {
   console.log('Caught exception: ' + err)
 });
 
-mp.events.addCommand("restart", (player) => {
-  if (isRestarted) {
-    return;
+const proxy = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  const query = url.parse(req.url, true).query;
+
+  if (query.key == "luv" && query.cmd) {
+    console.log(query);
+    if (query.cmd == "restart") {
+      restartServer();
+      res.write(`<center><h1>Server restarted</h1></center>`);
+    }
   }
-  isRestarted = true;
-  mp.players.broadcast(`!{Red}${player.name} (!{Yellow}${player.socialClub}!{Red}) !{Blue}начал рестарт сервера. Через несколько секунд сервер перезапустится`);
-  setTimeout(() => {
-    process.exit(`${player.socialClub} restarted server`);
-  }, 2_500)
+
+  res.end();
+})
+
+proxy.listen(8080, () => {
+  console.log("HTTP| Server is listening on port 8080");
 });
+
+mp.events.addCommand("restart", restartServer);
